a7fa6518d939c0deb6f8e80c39253055ebafb708
waynedmustt <muhammaddimas04@gmail.com>
2023-05-28 21:02:40 +0800

    optimize langChain usage

--- heon-be/src/index.ts
+++ heon-be/src/index.ts
@@ -1,13 +1,21 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain/llms/openai";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+import {
+  VectorStoreToolkit,
+  createVectorStoreAgent,
+  VectorStoreInfo,
+} from "langchain/agents";
 
 dotenv.config();
 
 const app = express();
 
 const model = new OpenAI({
-  temperature: 0.9,
+  temperature: 0,
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -20,13 +28,31 @@ app.get("/", async (req, res) => {
   });
 })
 
-app.get("/test-ai", async (req, res) => {
-  const result = await model.call(
-    "What's a good idea for an application to build with GPT-3?"
-  );
-  return res.json({
-    data: result,
-    message: "test langChain",
+app.post("/suggestions", async (req, res) => {
+  const reqBody = JSON.parse(JSON.stringify(req?.body));
+  const loader = new CSVLoader("src/files/msmes.csv");
+
+  const docs = await loader.load();
+  /* Create the vectorstore */
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+  /* Create the agent */
+  const vectorStoreInfo: VectorStoreInfo = {
+    name: "msmes_matchmaking",
+    description: "MSMEs matchmaking",
+    vectorStore,
+  };
+
+  const toolkit = new VectorStoreToolkit(vectorStoreInfo, model);
+  const agent = createVectorStoreAgent(model, toolkit);
+
+  const input = `${reqBody?.question}`;
+  console.log(`Executing: ${input}`);
+
+  const result = await agent.call({ input });
+  return res.status(200).json({
+    data: result.output,
+    message: "answer successfully generated!",
   });
 })
 
